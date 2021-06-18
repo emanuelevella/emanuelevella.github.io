a0c9022637918514e8bba9c68a8478470f278592
manyuvll <manyuvella@gmail.com>
2021-06-19 02:42:29 +0900

    add: dependencies, styled component theme manager

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from './features/navbar/Navbar'
+import styled, { createGlobalStyle } from 'styled-components';
+import { ThemeProvider } from "styled-components"
+import { lightTheme, darkTheme } from './helpers/themes'
+
+const GlobalStyle = createGlobalStyle`
+  body, html  {
+    background-color: rgb(255, 255, 255);;
+    overflow: hidden;
+    height:100vh;
+    width: 100vw;
+    margin: 0;
+  }
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-            <Route exact path="/">
-              Home
-            </Route>
-            <Route path="/buy">
-              BUY
-            </Route>
-        </Switch>
-      </Router>
-    </div>
+    <ThemeProvider theme={lightTheme}>
+      <Wrapper>
+        <GlobalStyle/>
+        <Navbar/>
+        <Router>
+          <Switch>
+              <Route exact path="/">
+                <span>Home</span>
+              </Route>
+              <Route path="/buy">
+                BUY
+              </Route>
+          </Switch>
+        </Router>
+      </Wrapper>
+    </ThemeProvider>
   );
 }
 
---
+++ src/helpers/themes.js
@@ -0,0 +1,13 @@
+
+export const lightTheme = {
+    bodyBg: "white",
+    headings: "black",
+    text: "#818181",
+    logoFilter: ""
+  }
+  export const darkTheme = {
+    bodyBg: "#141a2c",
+    headings: "white",
+    text: "#c1c1c1",
+    logoFilter: "invert(1)"
+  }
