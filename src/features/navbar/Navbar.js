2c2f00ff378da7a0ae625cd3deca41c73ce4a087
manyuvll <manyuvella@gmail.com>
2021-06-22 02:34:20 +0900

    add: avatar on navbar

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import logo from '../../assets/logo.svg'
+import avatar from '../../assets/avatar.png'
 import menu from '../../assets/icons/menu.svg'
 import {isMobile} from 'react-device-detect';
 import SwitchDarkMode from '../toggle/switchDarkMode/SwitchDarkMode'
@@ -29,6 +30,15 @@ const Buy = styled.div`
     margin-right: 20px;
 `
 
+const TextHover = styled.span`
+    &:hover {
+        background: linear-gradient(45deg, rgba(231,0,138,1) 0%, rgba(255,0,58,1) 100%);
+        background-clip: text;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+    }
+`
+
 const Menu = styled.img`
     height: auto;
     width: 3rem;
@@ -37,12 +47,24 @@ const Menu = styled.img`
     filter: ${props => props.theme.menuFilter};
 `
 
+const Avatar = styled.img`
+    height: 40px;
+    width: 40px;
+    object-fit: contain;
+    border-radius: 50%;
+    margin-right: 20px;
+    &:hover {
+        cursor: pointer;
+    }
+`
+
 export default function Navbar() {
     const mobileMenu = () => {
         if(isMobile) return <Menu src={menu}></Menu>
         return (
             <>
-            <Buy><Link to='buy'>Buy</Link></Buy>
+            <Buy><Link to='buy'><TextHover>Buy</TextHover></Link></Buy>
+            <Avatar src={avatar}></Avatar>
             <SwitchDarkMode/>
             </>
         )
